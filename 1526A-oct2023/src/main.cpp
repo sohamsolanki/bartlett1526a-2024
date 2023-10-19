90724ed4ea8154c68cc8953647e28df815aeb209
Soham Solanki <sohamsolanki@outlook.com>
2023-10-19 11:41:36 -0500

    Exponential code #2

--- 1526A-oct2023/src/main.cpp
+++ 1526A-oct2023/src/main.cpp
@@ -70,19 +70,40 @@ void autonomous(void) {
 /*                                                                           */
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
-void setLeftDrive (vex::directionType type, int percentage) {
+/* void setLeftDrive (vex::directionType type, int percentage) {
   LeftMotor.spin(type, percentage, vex::velocityUnits::pct);
 }
 
 void setRightDrive (vex::directionType type, int percentage) {
+  RightMotor.spin(type, percentage, vex::velocityUnits::pct);
+} */
+
+void setLeftDriveExpo (vex::directionType type, int percentage) {
+  if (percentage >= 0) {
+    percentage = 1.2*pow(1.043, percentage) - 1.2 + 0.2 * percentage;
+  } else {
+    percentage = -percentage;
+    percentage = 1.2*pow(1.043, percentage) - 1.2 + 0.2 * percentage;
+    percentage = -percentage;
+  }
+  LeftMotor.spin(type, percentage, vex::velocityUnits::pct);
+}
+void setRightDriveExpo (vex::directionType type, int percentage) {
+  if (percentage >= 0) {
+    percentage = 1.2*pow(1.043, percentage) - 1.2 + 0.2 * percentage;
+  } else {
+    percentage = -percentage;
+    percentage = 1.2*pow(1.043, percentage) - 1.2 + 0.2 * percentage;
+    percentage = -percentage;
+  }
   RightMotor.spin(type, percentage, vex::velocityUnits::pct);
 }
 
 void usercontrol(void) {
   // User control code here, inside the loop
   while (1) {
-    setLeftDrive (vex::directionType::fwd, Controller1.Axis3.value());
-    setRightDrive (vex::directionType::fwd, Controller1.Axis2.value());
+    setLeftDriveExpo (vex::directionType::fwd, Controller1.Axis3.value());
+    setRightDriveExpo (vex::directionType::fwd, Controller1.Axis2.value());
 
     if (Controller1.ButtonR2.pressing()) {
       Motor5.setVelocity(100, percent);
