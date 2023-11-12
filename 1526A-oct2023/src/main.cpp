656335ee341a4695303f6583854a891e1c4c2762
Soham Solanki <sohamsolanki@outlook.com>
2023-11-12 09:52:50 -0600

    Cleaned up code + added comments

--- 1526A-oct2023/src/main.cpp
+++ 1526A-oct2023/src/main.cpp
@@ -1,60 +1,30 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       VEX                                                       */
-/*    Created:      Thu Sep 26 2019                                           */
-/*    Description:  Competition Template                                      */
+/*    Author:       Soham Solanki and Raiyan Hasan                            */
+/*    Created:      October 15, 2023                                          */
+/*    Description:  1526A - November Code                                     */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
-
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// RightMotor           motor_group   1, 2            
-// LeftMotor            motor_group   3, 4            
-// Motor5               motor         5               
-// Motor6               motor         6               
-// Right2               motor         8               
-// Left2                motor         7               
-// Intake               digital_out   A               
-// ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
 
 using namespace vex;
 
-// A global instance of competition
 competition Competition;
-
-// define your global instances of motors and other devices here
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
-/*                                                                           */
-/*  You may want to perform some actions before the competition starts.      */
-/*  Do them in the following function.  You must return from this function   */
-/*  or the autonomous and usercontrol tasks will not be started.  This       */
-/*  function is only called once after the V5 has been powered on and        */
-/*  not every time that the robot is disabled.                               */
 /*---------------------------------------------------------------------------*/
 
 void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-
-  // All activities that occur before the competition starts
-  // Example: clearing encoders, setting servo positions, ...
+  // All activities that occur before the competition starts goes here
 }
 
 /*---------------------------------------------------------------------------*/
-/*                                                                           */
 /*                              Autonomous Task                              */
-/*                                                                           */
-/*  This task is used to control your robot during the autonomous phase of   */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
 void autonomous(void) {
@@ -64,22 +34,10 @@ void autonomous(void) {
 }
 
 /*---------------------------------------------------------------------------*/
-/*                                                                           */
 /*                              User Control Task                            */
-/*                                                                           */
-/*  This task is used to control your robot during the user control phase of */
-/*  a VEX Competition.                                                       */
-/*                                                                           */
-/*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
-/* void setLeftDrive (vex::directionType type, int percentage) {
-  LeftMotor.spin(type, percentage, vex::velocityUnits::pct);
-}
 
-void setRightDrive (vex::directionType type, int percentage) {
-  RightMotor.spin(type, percentage, vex::velocityUnits::pct);
-} */
-
+// EXPONENTIAL DRIVE CODE FUNCTIONS //
 void setLeftDriveExpo (vex::directionType type, int percentage) {
   if (percentage >= 0) {
     percentage = 1.2*pow(1.043, percentage) - 1.2 + 0.2 * percentage;
@@ -103,6 +61,7 @@ void setRightDriveExpo (vex::directionType type, int percentage) {
   Left2.spin(type, percentage, vex::velocityUnits::pct);
 }
 
+// FUNCTIONS FOR INTAKE //
 bool switchOn = false;
 bool switchOff = true;
 
@@ -116,13 +75,13 @@ void intakeoff(void) {
   switchOff = true;
 }
 
-
 void usercontrol(void) {
-  // User control code here, inside the loop
   while (1) {
+    // calling drivecode functions //
     setLeftDriveExpo (vex::directionType::fwd, Controller1.Axis3.value());
     setRightDriveExpo (vex::directionType::fwd, Controller1.Axis2.value());
 
+    // catapult code //
     if (Controller1.ButtonL2.pressing()) {
       Motor5.setVelocity(100, percent);
       Motor6.setVelocity(100, percent);
@@ -133,6 +92,7 @@ void usercontrol(void) {
       Motor6.stop();
     }
 
+    // intake code //
     if(switchOn == true){
       Intake.set(true);
     }
@@ -141,26 +101,18 @@ void usercontrol(void) {
       Intake.set(false);
     }
 
+    // calling intake function // 
     Controller1.ButtonA.pressed(intakeon);
     Controller1.ButtonY.pressed(intakeoff);
  
-    wait(20, msec); // Sleep the task for a short amount of time to
-                    // prevent wasted resources.
+    wait(20, msec); 
   }
 }
 
-//
-// Main will set up the competition functions and callbacks.
-//
 int main() {
-  // Set up callbacks for autonomous and driver control periods.
   Competition.autonomous(autonomous);
   Competition.drivercontrol(usercontrol);
-
-  // Run the pre-autonomous function.
   pre_auton();
-
-  // Prevent main from exiting with an infinite loop.
   while (true) {
     wait(100, msec);
   }
