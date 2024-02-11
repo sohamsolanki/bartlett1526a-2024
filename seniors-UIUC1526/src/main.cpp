80c40e5f60e5058c8d50de4326dd09be78fee884
Soham Solanki <sohamsolanki@outlook.com>
2024-02-11 13:27:43 -0600

    Code change #132933
    tired

--- seniors-UIUC1526/src/main.cpp
+++ seniors-UIUC1526/src/main.cpp
@@ -127,14 +127,9 @@ void usercontrol(void) {
     setLeftDriveExpo (vex::directionType::fwd, Controller1.Axis3.value());
     setRightDriveExpo (vex::directionType::fwd, Controller1.Axis2.value());
 
-    /* vex::brain Brain;
-    int count = 0;
-    vex::Gif gif("utopia.gif", 0, 0 );
-  
-    Brain.Screen.printAt(5, 230, "render %d", count++ );
-    Brain.Screen.render();
+    vex::brain Brain;
 
-    wait(20, msec); */
+    wait(20, msec);
   }
 }
 // do not touch!
