3452311f9a47f17cd58a4069d706549f14143172
Soham Solanki <sohamsolanki@outlook.com>
2024-03-16 13:22:46 -0500

    uhhh idk if this PID will work

---
+++ 1526a-STATE/src/main.cpp
@@ -0,0 +1,212 @@
+#include "vex.h"
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Left1                motor         10              
+// Left2                motor         9               
+// Left3                motor_group   8, 7            
+// Right1               motor         20              
+// Right2               motor         19              
+// Right3               motor_group   18, 17          
+// Intake               motor         16              
+// Sol1                 digital_out   A               
+// Sol2                 digital_out   B               
+// Sol3                 digital_out   C               
+// Sol4                 digital_out   D               
+// Sol5                 digital_out   E               
+// Sol6                 digital_out   F               
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+using namespace vex;
+competition Competition;
+
+Drive chassis(
+ZERO_TRACKER_NO_ODOM,
+
+// Left Motors:
+motor_group(Left1, Left2, Left3),
+
+//Right Motors:
+motor_group(Right1, Right2, Right3),
+
+//Specify the PORT NUMBER of your inertial sensor, in PORT format (i.e. "PORT1", not simply "1"):
+PORT1,
+
+//Input your wheel diameter. (4" omnis are actually closer to 4.125"):
+2.75,
+
+//External ratio, must be in decimal, in the format of input teeth/output teeth.
+//If your motor has an 84-tooth gear and your wheel has a 60-tooth gear, this value will be 1.4.
+//If the motor drives the wheel directly, this value is 1:
+0.6,
+
+//Gyro scale, this is what your gyro reads when you spin the robot 360 degrees.
+//For most cases 360 will do fine here, but this scale factor can be very helpful when precision is necessary.
+360,
+
+/*---------------------------------------------------------------------------*/
+/*                                  PAUSE!                                   */
+/*                                                                           */
+/*  The rest of the drive constructor is for robots using POSITION TRACKING. */
+/*  If you are not using position tracking, leave the rest of the values as  */
+/*  they are.                                                                */
+/*---------------------------------------------------------------------------*/
+
+//If you are using ZERO_TRACKER_ODOM, you ONLY need to adjust the FORWARD TRACKER CENTER DISTANCE.
+
+//FOR HOLONOMIC DRIVES ONLY: Input your drive motors by position. This is only necessary for holonomic drives, otherwise this section can be left alone.
+//LF:      //RF:    
+PORT1,     -PORT2,
+
+//LB:      //RB: 
+PORT3,     -PORT4,
+
+//If you are using position tracking, this is the Forward Tracker port (the tracker which runs parallel to the direction of the chassis).
+//If this is a rotation sensor, enter it in "PORT1" format, inputting the port below.
+//If this is an encoder, enter the port as an integer. Triport A will be a "1", Triport B will be a "2", etc.
+3,
+
+//Input the Forward Tracker diameter (reverse it to make the direction switch):
+2.75,
+
+//Input Forward Tracker center distance (a positive distance corresponds to a tracker on the right side of the robot, negative is left.)
+//For a zero tracker tank drive with odom, put the positive distance from the center of the robot to the right side of the drive.
+//This distance is in inches:
+-2,
+
+//Input the Sideways Tracker Port, following the same steps as the Forward Tracker Port:
+1,
+
+//Sideways tracker diameter (reverse to make the direction switch):
+-2.75,
+
+//Sideways tracker center distance (positive distance is behind the center of the robot, negative is in front):
+5.5
+
+);
+
+int current_auton_selection = 0;
+bool auto_started = false;
+
+void pre_auton(void) {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  default_constants();
+
+  while(auto_started == false){            //Changing the names below will only change their names on the
+    Brain.Screen.clearScreen();            //brain screen for auton selection.
+    switch(current_auton_selection){       //Tap the brain screen to cycle through autons.
+      case 0:
+        Brain.Screen.printAt(50, 50, "Drive Test");
+        break;
+      case 1:
+        Brain.Screen.printAt(50, 50, "Drive Test");
+        break;
+      case 2:
+        Brain.Screen.printAt(50, 50, "Turn Test");
+        break;
+      case 3:
+        Brain.Screen.printAt(50, 50, "Swing Test");
+        break;
+      case 4:
+        Brain.Screen.printAt(50, 50, "Full Test");
+        break;
+      case 5:
+        Brain.Screen.printAt(50, 50, "Odom Test");
+        break;
+      case 6:
+        Brain.Screen.printAt(50, 50, "Tank Odom Test");
+        break;
+      case 7:
+        Brain.Screen.printAt(50, 50, "Holonomic Odom Test");
+        break;
+    }
+    if(Brain.Screen.pressing()){
+      while(Brain.Screen.pressing()) {}
+      current_auton_selection ++;
+    } else if (current_auton_selection == 8){
+      current_auton_selection = 0;
+    }
+    task::sleep(10);
+  }
+}
+
+void autonomous(void) {
+  auto_started = true;
+  switch(current_auton_selection){  
+    case 0:
+      drive_test(); //This is the default auton, if you don't select from the brain.
+      break;        //Change these to be your own auton functions in order to use the auton selector.
+    case 1:         //Tap the screen to cycle through autons.
+      drive_test();
+      break;
+    case 2:
+      turn_test();
+      break;
+    case 3:
+      swing_test();
+      break;
+    case 4:
+      full_test();
+      break;
+    case 5:
+      odom_test();
+      break;
+    case 6:
+      tank_odom_test();
+      break;
+    case 7:
+      holonomic_odom_test();
+      break;
+ }
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+void usercontrol(void) {
+  // User control code here, inside the loop
+  while (1) {
+    // This is the main execution loop for the user control program.
+    // Each time through the loop your program should update motor + servo
+    // values based on feedback from the joysticks.
+
+    // ........................................................................
+    // Insert user code here. This is where you use the joystick values to
+    // update your motors, etc.
+    // ........................................................................
+
+    //Replace this line with chassis.control_tank(); for tank drive 
+    //or chassis.control_holonomic(); for holo drive.
+    chassis.control_arcade();
+
+    wait(20, msec); // Sleep the task for a short amount of time to
+                    // prevent wasted resources.
+  }
+}
+
+//
+// Main will set up the competition functions and callbacks.
+//
+int main() {
+  // Set up callbacks for autonomous and driver control periods.
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  // Run the pre-autonomous function.
+  pre_auton();
+
+  // Prevent main from exiting with an infinite loop.
+  while (true) {
+    wait(100, msec);
+  }
+}
---
+++ 1526a-STATE/src/robot-config.cpp
@@ -0,0 +1,41 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor Left1 = motor(PORT10, ratio6_1, false);
+motor Left2 = motor(PORT9, ratio18_1, false);
+motor Left3MotorA = motor(PORT8, ratio6_1, false);
+motor Left3MotorB = motor(PORT7, ratio6_1, true);
+motor_group Left3 = motor_group(Left3MotorA, Left3MotorB);
+motor Right1 = motor(PORT20, ratio6_1, true);
+motor Right2 = motor(PORT19, ratio18_1, true);
+motor Right3MotorA = motor(PORT18, ratio6_1, true);
+motor Right3MotorB = motor(PORT17, ratio6_1, false);
+motor_group Right3 = motor_group(Right3MotorA, Right3MotorB);
+motor Intake = motor(PORT16, ratio6_1, false);
+digital_out Sol1 = digital_out(Brain.ThreeWirePort.A);
+digital_out Sol2 = digital_out(Brain.ThreeWirePort.B);
+digital_out Sol3 = digital_out(Brain.ThreeWirePort.C);
+digital_out Sol4 = digital_out(Brain.ThreeWirePort.D);
+digital_out Sol5 = digital_out(Brain.ThreeWirePort.E);
+digital_out Sol6 = digital_out(Brain.ThreeWirePort.F);
+controller Controller1 = controller(primary);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
