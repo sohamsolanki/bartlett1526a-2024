54c4e0178f5dfc7448f4fe1f34536d2fd94ed461
Soham Solanki <sohamsolanki@outlook.com>
2024-03-19 20:21:55 -0500

    Update autons.cpp

--- 1526a-STATE/src/autons.cpp
+++ 1526a-STATE/src/autons.cpp
@@ -19,7 +19,9 @@ void odom_constants(){
 void drive_test(){
   chassis.drive_distance(10);
   chassis.drive_distance(-10);
-  chassis.turn_to_angle(1);
+  //chassis.turn_to_angle(1);
+  chassis.left_swing_to_angle(90);
+  chassis.right_swing_to_angle(0);
 }
 
 /* void turn_test(){
